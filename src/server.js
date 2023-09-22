f7208893bb01f239cbbaa74c022ddf6423cbd967
Filladen <fiiv22ab@student.cbs.dk>
2023-09-22 09:36:43 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use("/customer", customerRoute);
 app.use("/cloudinary", cloudinaryRoute);
 
 app.get("/", (req, res) => {
-  res.send("Hej Thomas!");
+  res.send("Det virker... nu kan siden tilgås via ip-adressen af alle!");
 });
 
 app.listen(3000, () => {
